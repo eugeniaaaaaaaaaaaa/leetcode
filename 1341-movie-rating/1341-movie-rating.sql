d2c5a05bee2f1dc4261b210352c2e58ca1c033f1
Eugenia <138609699+eugeniaaaaaaaaaaaa@users.noreply.github.com>
2026-02-10 13:49:42 +0500

    Time: 242 ms (73.3%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,25 +1,19 @@
 -- Write your PostgreSQL query statement below
-WITH greatest_ranker AS 
-    (SELECT u.name AS name
-    FROM MovieRating r
-    LEFT JOIN Users u
-    ON r.user_id = u.user_id
-    GROUP BY name
-    ORDER BY COUNT(*) DESC, name
-    LIMIT 1),
+(SELECT u.name AS results
+FROM MovieRating r
+LEFT JOIN Users u
+ON r.user_id = u.user_id
+GROUP BY u.name
+ORDER BY COUNT(*) DESC, u.name
+LIMIT 1)
 
-    highest_rating_movie AS
-    (SELECT m.title AS title
-    FROM MovieRating r
-    LEFT JOIN Movies m
-    ON r.movie_id = m.movie_id
-    WHERE created_at BETWEEN '2020-02-01'::DATE AND '2020-02-29'::DATE
-    GROUP BY title
-    ORDER BY AVG(r.rating) DESC, title
-    LIMIT 1)
-
-SELECT name AS results
-FROM greatest_ranker
 UNION ALL 
-SELECT title AS results
-FROM highest_rating_movie;
+
+(SELECT m.title AS results
+FROM MovieRating r
+LEFT JOIN Movies m
+ON r.movie_id = m.movie_id
+WHERE created_at BETWEEN '2020-02-01'::DATE AND '2020-02-29'::DATE
+GROUP BY m.title
+ORDER BY AVG(r.rating) DESC, m.title
+LIMIT 1);
